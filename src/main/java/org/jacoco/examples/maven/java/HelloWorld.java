564c67ed9aa41e1ed0becef1142649ce6ad8ae3c
aforte15 <awojtowi@ryerson.ca>
2017-02-24 18:20:23 -0500

    Create HelloWorld.java

---
+++ src/main/java/org/jacoco/examples/maven/java/HelloWorld.java
@@ -0,0 +1,14 @@
+package org.jacoco.examples.maven.java;
+
+public class HelloWorld {
+	
+	public String getMessage(boolean bigger) {
+		//blatent_error();
+		if (bigger) {
+			return "Hello Universe!";
+		} else {
+			return "Hello World!";
+		}
+	}
+
+}
